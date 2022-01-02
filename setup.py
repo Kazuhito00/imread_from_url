da31ab060089642073fdd276e3734df0a301da08
KazuhitoTakahashi <shigeki.takahashi.dev@gmail.com>
2022-01-03 02:41:22 +0900

    Version 0.1.2 -> 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='imread_from_url',
-    version='0.1.2',
+    version='0.1.3',
     license='Apache License, Version 2.0',
     description=
     'Read the image from the specified URL and enable it to be handled by OpenCV',
@@ -13,6 +13,6 @@ setup(
         'opencv-python>=3.4.2',
         'Pillow>=6.1.0',
         'requests>=2.22.0',
-        'fake-useragent>=0.1.11',        
+        'fake-useragent>=0.1.11',
     ],
 )
